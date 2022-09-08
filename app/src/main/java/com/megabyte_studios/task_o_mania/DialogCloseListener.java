65f5bab29f21002344b1b8a5dfe10bfa304bbfdc
YTW7 <myahya96@gmail.com>
2022-09-09 00:53:58 +0530

    Add File

---
+++ app/src/main/java/com/megabyte_studios/task_o_mania/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.megabyte_studios.task_o_mania;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
